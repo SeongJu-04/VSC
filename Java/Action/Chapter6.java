f922893f4cc955f7fe7a5c9c703524f4e5a09066
홍성주 <hxs04107@gmail.com>
2026-04-12 15:35:00 +0900

    2026.04.11 공부

---
+++ Java/Action/Chapter6.java
@@ -0,0 +1,71 @@
+package Action;
+
+/*
+class Vehicle {
+    private int speed; 
+    private int direction;
+
+    public Vehicle() {}
+
+    public int getSpeed() { return speed;}
+    public int getDirection() {return direction;}
+    public void setSpeed(int speed) {this.speed = speed;}
+    public void setDirection(int direction) {this.direction = direction;}
+    
+
+}
+
+class Truck extends Vehicle {
+    int currentLoad; //defalt 접근자로 이 경우 여기 파일 안에서는 모두가 사용가능 but 프로그램상 손 쉽게 변경이 가능하기에 private을 권장하는 것 
+
+}
+
+public class Chapter6 {
+        public static void main(String args[]){
+
+ 
+    }
+}
+
+
+class Person{
+    private String name;
+    private int age;
+    public Person() {
+        
+    }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    protected String getName() {return name;}
+    protected int getAge() {return age;}
+    
+    public void introduce() {
+        System.out.println("Person");
+    }
+}
+
+
+class Student extends Person{
+    private int studentId;
+    public Student() {super();}
+    public Student(String name,int age) {
+        super(name,age);
+    }
+    @Override
+    public void introduce() {
+        System.out.println("내 이름은 " + getName() + "이고, 나이는" + getAge() +"살입니다.");
+    }
+}
+
+public class Chapter6 {
+    public static void main(String args[]) {
+        Student obj = new Student("홍성주", 23);
+        obj.introduce();
+    }
+}
+
+*/
+
